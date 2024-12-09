ce6363d22dbcb80d0299e000d982589c596f31eb
Paula Cadena <paulacadena@Mac.attlocal.net>
2024-12-08 23:21:55 -0600

    Almost ready

---
+++ src/pages.py
@@ -0,0 +1,25 @@
+from clean import countries_dict
+from plots import migration_flow, migration_rate
+import panel as pn
+pn.extension('vega')
+
+#Plots Country Page + Widgets
+select = pn.widgets.Select(name='Year', 
+                           options = [1990,1995,2000,2005,2010,2015,2020])
+interactive_flow= pn.bind(migration_flow, selected_year=select)
+dict_autocomplete = pn.widgets.AutocompleteInput(name='Search country', 
+                                                 options=countries_dict(), 
+                                                 case_sensitive = False)
+plots_country = pn.Column(select, interactive_flow)
+
+plots_country.save("/Users/paulacadena/Git-Hub/CAPP30239-IP/www/plots_country.html", 
+                   embed=True)
+
+#Plots Region Page + Widgets
+
+interactive_plot = pn.bind(migration_rate, selected_year=select)
+select_row = pn.Row(pn.Spacer(width=800), select, sizing_mode='stretch_width')
+plots_region = pn.Column(select_row, interactive_plot)
+
+plots_region.save("/Users/paulacadena/Git-Hub/CAPP30239-IP/www/plots_region.html", 
+                   embed=True)
